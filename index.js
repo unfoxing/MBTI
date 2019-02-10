5115a5d7931d3e57bcbe00919b75d63703936236
Mario Rizki <mariorizki07@gmail.com>
2019-02-11 01:14:34 +0700

    do some refactor

--- index.js
+++ index.js
@@ -1,60 +1,60 @@
 const MBTI = {
-  choose_E_or_1: () => {
-    ektrovert = document.getElementsByName('category1')
+  choose_E_or_I: () => {
+    const extrovert = document.getElementsByName('category1');
+    const introvert = document.getElementsByName('category1');
 
-    introvert = document.getElementsByName('category1')
-
-    if (ektrovert[0].checked == true) {
-      return 'E'
+    if (extrovert[0].checked == true) {
+      return 'E';
     } else if (introvert[1].checked == true) {
-      return '1'
+      return 'I';
     }
   },
-  choose_S_or_I: () => {
-    sensing = document.getElementsByName('category2')
 
-    intuition = document.getElementsByName('category2')
+  choose_S_or_I: () => {
+    const sensing = document.getElementsByName('category2');
+    const intuition = document.getElementsByName('category2');
 
     if (sensing[0].checked == true) {
-      return 'S'
+      return 'S';
     } else if (intuition[1].checked == true) {
-      return 'I'
+      return 'N';
     }
   },
+
   choose_T_or_F: () => {
-    thingking = document.getElementsByName('category3')
+    const thinking = document.getElementsByName('category3');
+    const feeling = document.getElementsByName('category3');
 
-    feeling = document.getElementsByName('category3')
-    // console.log(introvert[1].checked)
-    if (thingking[0].checked == true) {
-      return 'T'
+    if (thinking[0].checked == true) {
+      return 'T';
     } else if (feeling[1].checked == true) {
-      return 'F'
+      return 'F';
     }
   },
-  choose_J_or_P: () => {
-    jugding = document.getElementsByName('category4')
-    perceiving = document.getElementsByName('category4')
 
-    if (jugding[0].checked == true) {
-      return 'J'
+  choose_J_or_P: () => {
+    const judging = document.getElementsByName('category4');
+    const perceiving = document.getElementsByName('category4');
+
+    if (judging[0].checked == true) {
+      return 'J';
     } else if (perceiving[1].checked == true) {
-      return 'P'
+      return 'P';
     }
   },
 
   resultMBTI: () => {
     if (
-      MBTI.choose_E_or_1() === undefined ||
+      MBTI.choose_E_or_I() === undefined ||
       MBTI.choose_S_or_I() === undefined ||
       MBTI.choose_T_or_F() === undefined ||
       MBTI.choose_J_or_P() === undefined
     ) {
-      alert('semua harus di isi')
+      alert('All question must be answered');
     } else {
       alert(
-        `hasil ${MBTI.choose_E_or_1()} ${MBTI.choose_S_or_I()} ${MBTI.choose_T_or_F()} ${MBTI.choose_J_or_P()}`
-      )
+        `Your personality type is ${MBTI.choose_E_or_I()} ${MBTI.choose_S_or_I()} ${MBTI.choose_T_or_F()} ${MBTI.choose_J_or_P()}`
+      );
     }
   }
-}
+};
