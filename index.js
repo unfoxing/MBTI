43e96dd495701dbf59247fb3a6c275c8a6fce692
ajinbramantio <ajinbramantio@gmail.com>
2019-02-11 06:04:51 +0700

    update choose_S_or_I to choose_S_or_N

--- index.js
+++ index.js
@@ -1,60 +1,60 @@
 const MBTI = {
   choose_E_or_I: () => {
-    const extrovert = document.getElementsByName('category1');
-    const introvert = document.getElementsByName('category1');
+    const extrovert = document.getElementsByName('category1')
+    const introvert = document.getElementsByName('category1')
 
     if (extrovert[0].checked == true) {
-      return 'E';
+      return 'E'
     } else if (introvert[1].checked == true) {
-      return 'I';
+      return 'I'
     }
   },
 
-  choose_S_or_I: () => {
-    const sensing = document.getElementsByName('category2');
-    const intuition = document.getElementsByName('category2');
+  choose_S_or_N: () => {
+    const sensing = document.getElementsByName('category2')
+    const intuition = document.getElementsByName('category2')
 
     if (sensing[0].checked == true) {
-      return 'S';
+      return 'S'
     } else if (intuition[1].checked == true) {
-      return 'N';
+      return 'N'
     }
   },
 
   choose_T_or_F: () => {
-    const thinking = document.getElementsByName('category3');
-    const feeling = document.getElementsByName('category3');
+    const thinking = document.getElementsByName('category3')
+    const feeling = document.getElementsByName('category3')
 
     if (thinking[0].checked == true) {
-      return 'T';
+      return 'T'
     } else if (feeling[1].checked == true) {
-      return 'F';
+      return 'F'
     }
   },
 
   choose_J_or_P: () => {
-    const judging = document.getElementsByName('category4');
-    const perceiving = document.getElementsByName('category4');
+    const judging = document.getElementsByName('category4')
+    const perceiving = document.getElementsByName('category4')
 
     if (judging[0].checked == true) {
-      return 'J';
+      return 'J'
     } else if (perceiving[1].checked == true) {
-      return 'P';
+      return 'P'
     }
   },
 
   resultMBTI: () => {
     if (
       MBTI.choose_E_or_I() === undefined ||
-      MBTI.choose_S_or_I() === undefined ||
+      MBTI.choose_S_or_N() === undefined ||
       MBTI.choose_T_or_F() === undefined ||
       MBTI.choose_J_or_P() === undefined
     ) {
-      alert('All question must be answered');
+      alert('All question must be answered')
     } else {
       alert(
-        `Your personality type is ${MBTI.choose_E_or_I()} ${MBTI.choose_S_or_I()} ${MBTI.choose_T_or_F()} ${MBTI.choose_J_or_P()}`
-      );
+        `Your personality type is ${MBTI.choose_E_or_I()} ${MBTI.choose_S_or_N()} ${MBTI.choose_T_or_F()} ${MBTI.choose_J_or_P()}`
+      )
     }
   }
-};
+}
